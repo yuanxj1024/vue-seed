8bc4973fa33a6205339a116dc494e08d96226fe6
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-10-09 12:49:39 +0800

    add  file

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -1,11 +1,13 @@
+/**
+ * 数据存储
+ */
 import user from 'modules/user.js';
 
-const Vuex = window.Vuex; // require('vuex');
+const Vuex = window.Vuex;
 
 const store = new Vuex.Store({
   modules: {
     user,
   },
 });
-console.log(user);
 module.exports = store;
